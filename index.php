1279286155bf9ed4ce50f10ecea2cff31021bb21
Glenn-Erik Sandbakken <glennerik@gmail.com>
2017-06-04 14:58:36 +0200

    Adding json as Content-Type in http header response

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
 $browser = get_browser(null, true);
 $browser['useragent'] = $_SERVER['HTTP_USER_AGENT'];
+header('Content-Type: application/json; charset=UTF-8');
 print_r(json_encode($browser));
