7b64f7ff80b31dbcf3c75e7c79057574c7e1d8ff
Glenn-Erik Sandbakken <glennerik@gmail.com>
2017-06-10 22:24:34 +0200

    Add support for rdns of any IP

--- rdns.php
+++ rdns.php
@@ -1,6 +1,16 @@
 <?php
-// TODO: Add suport for any IP Address, not just remote
-$hostName = gethostbyaddr(isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR']);
-header('Content-Type: text/plain; charset=UTF-8');
-print $hostName;
+if(isset($_GET['ip']) && isValidIp($_GET['ip'])) {
+  $hostName = gethostbyaddr($_GET['ip']);
+  header('Content-Type: text/plain; charset=UTF-8');
+  print $hostName;
+}else{
+  $hostName = gethostbyaddr(isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR']);
+  header('Content-Type: text/plain; charset=UTF-8');
+  print $hostName;
+}
+
+function isValidIp($ip) {
+  if(substr_count($ip, '.') != 4) return false;
+  return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE);
+}
 ?>
