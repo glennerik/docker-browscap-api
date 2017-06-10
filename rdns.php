2e8c8d577052a4baa2138d9b01aeb0fa987b0afd
Glenn-Erik Sandbakken <glennerik@gmail.com>
2017-06-10 22:33:38 +0200

    Fix a bug

--- rdns.php
+++ rdns.php
@@ -1,8 +1,15 @@
 <?php
-if(isset($_GET['ip']) && isValidIp($_GET['ip'])) {
-  $hostName = gethostbyaddr($_GET['ip']);
-  header('Content-Type: text/plain; charset=UTF-8');
-  print $hostName;
+
+if(isset($_GET['ip'])) {
+  $ip = isValidIp($_GET['ip']);
+  if(!$ip) {
+    header('Content-Type: text/plain; charset=UTF-8');
+    print 'Not a valid IP Address';    
+  }else{
+    $hostName = gethostbyaddr($ip);
+    header('Content-Type: text/plain; charset=UTF-8');
+    print $hostName;
+  }
 }else{
   $hostName = gethostbyaddr(isset($_SERVER['HTTP_CF_CONNECTING_IP']) ? $_SERVER['HTTP_CF_CONNECTING_IP'] : $_SERVER['REMOTE_ADDR']);
   header('Content-Type: text/plain; charset=UTF-8');
@@ -10,7 +17,7 @@ if(isset($_GET['ip']) && isValidIp($_GET['ip'])) {
 }
 
 function isValidIp($ip) {
-  if(substr_count($ip, '.') != 4) return false;
+  if(substr_count($ip, '.') != 3) return false;
   return filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE);
 }
 ?>
